799756880c9641ac397610fe6094836e00835596
vanjazeli <vanjazeli@gmail.com>
2024-05-31 14:38:42 +0200

    fix: get rid of a display png sequence bug

--- js/pngSequence.js
+++ js/pngSequence.js
@@ -1,51 +1,54 @@
 const pngSequence = {
-  animationBlock: document.querySelector('.js-scrollbox-animation'),
-  frames: document.querySelectorAll('.js-scrollbox-frame'),
+	animationBlock: document.querySelector('.js-scrollbox-animation'),
+	frames: document.querySelectorAll('.js-scrollbox-frame'),
 
-  currentFrame: { value: 0 },
+	currentFrame: { value: 0 },
 
-  offset: 0,
+	offset: 0,
 
-  init: function () {
-    this.initialSetup();
-    this.detectScroll();
-  },
+	init: function () {
+		this.initialSetup();
+		this.detectScroll();
+	},
 
-  initialSetup: function () {
-    this.frames[0].style.opacity = '1';
-  },
+	initialSetup: function () {
+		this.frames.forEach((frame) => {
+			frame.style.visibility = 'hidden';
+		});
+		this.frames[0].style.visibility = 'visible';
+	},
 
-  detectScroll: function () {
-    window.addEventListener('scroll', () => {
-      const rect = this.animationBlock.getBoundingClientRect();
-      const rectTop = document.documentElement.scrollTop - window.innerHeight;
-      const scrollTop = rect.top + rectTop + this.offset;
+	detectScroll: function () {
+		window.addEventListener('scroll', () => {
+			const rect = this.animationBlock.getBoundingClientRect();
+			const rectTop = document.documentElement.scrollTop - window.innerHeight;
+			const scrollTop = rect.top + rectTop + this.offset;
 
-      const animationBlockHeight = this.animationBlock.offsetHeight;
+			const animationBlockHeight = this.animationBlock.offsetHeight;
 
-      const scrollBottom = scrollTop + animationBlockHeight + window.innerHeight;
-      const currentScroll = window.scrollY;
+			const scrollBottom = scrollTop + animationBlockHeight + window.innerHeight;
+			const currentScroll = window.scrollY;
 
-      const isInViewport = currentScroll >= scrollTop && currentScroll <= scrollBottom;
+			const isInViewport = currentScroll >= scrollTop && currentScroll <= scrollBottom;
 
-      if (isInViewport) {
-        const rangePx = scrollBottom - scrollTop;
-        const currPositionPercent = (currentScroll - scrollTop) / rangePx;
+			if (isInViewport) {
+				const rangePx = scrollBottom - scrollTop;
+				const currPositionPercent = (currentScroll - scrollTop) / rangePx;
 
-        const nextFrame = Math.round(currPositionPercent * 90);
+				const nextFrame = Math.round(currPositionPercent * 90);
 
-        gsap.to(this.currentFrame, {
-          value: nextFrame,
-          onUpdate: () => {
-            this.frames.forEach((frame) => {
-              frame.style.opacity = '0';
-            });
-            this.frames[Math.round(this.currentFrame.value)].style.opacity = '1';
-          },
-        });
-      }
-    });
-  },
+				gsap.to(this.currentFrame, {
+					value: nextFrame,
+					onUpdate: () => {
+						this.frames.forEach((frame) => {
+							frame.style.visibility = 'hidden';
+						});
+						this.frames[Math.round(this.currentFrame.value)].style.visibility = 'visible';
+					},
+				});
+			}
+		});
+	},
 };
 
 export default pngSequence;
