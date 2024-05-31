6ccd8215672b01073783143b5d4cae03a0dd2949
vanjazeli <vanjazeli@gmail.com>
2024-05-31 08:42:28 +0200

    feat: add scrolling png sequence animation

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
+import pngSequence from './pngSequence.js';
+
 // Homepage Product Slider
-let thumbsSwiper = new Swiper(".product-slider .thumbs-slider .swiper", {
+let thumbsSwiper = new Swiper('.product-slider .thumbs-slider .swiper', {
   slidesPerView: 4,
 });
 
-let productSwiper = new Swiper(".product-slider .slider .swiper", {
+let productSwiper = new Swiper('.product-slider .slider .swiper', {
   thumbs: {
     swiper: thumbsSwiper,
   },
 });
+
+// modules
+pngSequence.init();
---
+++ js/pngSequence.js
@@ -0,0 +1,51 @@
+const pngSequence = {
+  animationBlock: document.querySelector('.js-scrollbox-animation'),
+  frames: document.querySelectorAll('.js-scrollbox-frame'),
+
+  currentFrame: { value: 0 },
+
+  offset: 0,
+
+  init: function () {
+    this.initialSetup();
+    this.detectScroll();
+  },
+
+  initialSetup: function () {
+    this.frames[0].style.opacity = '1';
+  },
+
+  detectScroll: function () {
+    window.addEventListener('scroll', () => {
+      const rect = this.animationBlock.getBoundingClientRect();
+      const rectTop = document.documentElement.scrollTop - window.innerHeight;
+      const scrollTop = rect.top + rectTop + this.offset;
+
+      const animationBlockHeight = this.animationBlock.offsetHeight;
+
+      const scrollBottom = scrollTop + animationBlockHeight + window.innerHeight;
+      const currentScroll = window.scrollY;
+
+      const isInViewport = currentScroll >= scrollTop && currentScroll <= scrollBottom;
+
+      if (isInViewport) {
+        const rangePx = scrollBottom - scrollTop;
+        const currPositionPercent = (currentScroll - scrollTop) / rangePx;
+
+        const nextFrame = Math.round(currPositionPercent * 90);
+
+        gsap.to(this.currentFrame, {
+          value: nextFrame,
+          onUpdate: () => {
+            this.frames.forEach((frame) => {
+              frame.style.opacity = '0';
+            });
+            this.frames[Math.round(this.currentFrame.value)].style.opacity = '1';
+          },
+        });
+      }
+    });
+  },
+};
+
+export default pngSequence;
